146bc96d1c200e7ca68753309cccd5641342e01a
Francis Nkansah <itmannanayaw@gmail.com>
2024-07-24 19:35:09 -0700

    changes made to footer and index page

--- include/footer.php
+++ include/footer.php
@@ -6,7 +6,7 @@
                             <h4 class="mb-4 text-white">Get In Touch</h4>
                             <a href=""><i class="fas fa-home me-2"></i> 123 Street, New York, USA</a>
                             <a href=""><i class="fas fa-envelope me-2"></i> info@example.com</a>
-                            <a href=""><i class="fas fa-phone me-2"></i> +012 345 67890</a>
+                            <a href=""><i class="fas fa-phone me-2"></i> +233 530 525 791</a>
                             <a href="" class="mb-3"><i class="fas fa-print me-2"></i> +012 345 67890</a>
                             <div class="d-flex align-items-center">
                                 <i class="fas fa-share fa-2x text-white me-2"></i>
