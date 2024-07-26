7ac416e2c7c781ff1aa259ba2b752678315893bf
Mohammed Alhassan <bigboss200535@gmail.com>
2024-07-26 18:43:22 +0000

    Newsletter update

--- database/bellas_db.sql
+++ database/bellas_db.sql
@@ -1,4 +1,4 @@
--- DROP DATABASE IF EXISTS `bellas_db`;
+DROP DATABASE IF EXISTS `bellas_db`;
 CREATE DATABASE IF NOT EXISTS `bellas_db`;
 USE `bellas_db`;
 
@@ -54,9 +54,9 @@ CREATE TABLE IF NOT EXISTS `system` (
   `company_name` varchar(200),
   `email` varchar(100),
   `telephone` varchar(20),
-  'api' varchar(10) DEFAULT 'Yes',
-  'email_alert' varchar(10) DEFAULT 'No',
-  'sms_alert', varchar(10), DEFAULT 'No',
+  `api` varchar(10) DEFAULT 'Yes',
+  `email_alert` varchar(10) DEFAULT 'No',
+  `sms_alert` varchar(10) DEFAULT 'No',
   `slogan` varchar(150),
   `added_by` varchar(100),
   `added_date` datetime DEFAULT current_timestamp(),
@@ -68,3 +68,20 @@ CREATE TABLE IF NOT EXISTS `system` (
   `archiveDate` datetime,
   PRIMARY KEY (`system_id`)
 )ENGINE=InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS `news_letter` (
+  `news_id` varchar(20),
+  `email` varchar(100),
+  `email_subscription` varchar(10) DEFAULT 'Yes',
+  `added_by` varchar(100),
+  `added_date` datetime DEFAULT current_timestamp(),
+  `updatedDate` datetime,
+  `updated_by` varchar(60),
+  `updated_notes` varchar(100),
+  `status` varchar(10) DEFAULT 'Active',
+  `archive` varchar(10) DEFAULT 'No',
+  `archiveDate` datetime,
+  PRIMARY KEY (`news_id`)
+)ENGINE=InnoDB;
+
