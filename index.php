16305bafd935777e4b39a05e83d2721152e6a6cc
Mohammed Alhassan <bigboss200535@gmail.com>
2024-07-27 16:09:26 +0000

    New update

--- index.php
+++ index.php
@@ -1005,15 +1005,15 @@
                     <div class="col-lg-6">
                         <h5 class="section-booking-title pe-3" style="color:#000000">Booking</h5>
                         <h1 class="text-black mb-4">Online Booking</h1>
-                        <p class="text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur maxime ullam esse fuga blanditiis accusantium pariatur quis sapiente, veniam doloribus praesentium? Repudiandae iste voluptatem fugiat doloribus quasi quo iure officia.
+                        <p class="text-black" style="color:#000000">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur maxime ullam esse fuga blanditiis accusantium pariatur quis sapiente, veniam doloribus praesentium? Repudiandae iste voluptatem fugiat doloribus quasi quo iure officia.
                         </p>
-                        <p class="text-black mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur maxime ullam esse fuga blanditiis accusantium pariatur quis sapiente, veniam doloribus praesentium? Repudiandae iste voluptatem fugiat doloribus quasi quo iure officia.
+                        <p class="text-black mb-4" style="color:#000000">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur maxime ullam esse fuga blanditiis accusantium pariatur quis sapiente, veniam doloribus praesentium? Repudiandae iste voluptatem fugiat doloribus quasi quo iure officia.
                         </p>
                         <a href="#" class="btn btn-light text-primary rounded-pill py-3 px-5 mt-2">Read More</a>
                     </div>
                     <div class="col-lg-6">
                         <h1 class="text-black mb-3" style="color:#000000">Book A Tour Deals</h1>
-                        <p class="text-black mb-4">Get <span class="text-danger">50% Off</span> On Your First Adventure Trip With Travela. Get More Deal Offers Here.</p>
+                        <p class="text-black mb-4" style="color:#000000">Get <span class="text-danger">50% Off</span> On Your First Adventure Trip With Travela. Get More Deal Offers Here.</p>
                         <form>
                             <div class="row g-3">
                                 <div class="col-md-6">
