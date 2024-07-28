23419f409e3652b105e820c0cf380022c174c4cd
Francis Nkansah <itmannanayaw@gmail.com>
2024-07-28 05:57:01 -0700

    homecare changes

--- homecare.php
+++ homecare.php
@@ -17,7 +17,7 @@
         <!-- Header Start -->
         <div class="container-fluid bg-breadcrumb">
             <div class="container text-center py-5" style="max-width: 900px;">
-                <h3 class="text-white display-3 mb-4">Our Home Care Services</h1>
+                <h3 class="text-white display-3 mb-4">Home Care</h1>
                 <ol class="breadcrumb justify-content-center mb-0">
                     <li class="breadcrumb-item"><a href="index.php">Home</a></li>
                     <li class="breadcrumb-item"><a href="#">Pages</a></li>
@@ -33,7 +33,15 @@
                 <div class="mx-auto text-center mb-5">
                     <h1 class="mb-4">Why choose us</h1>
                     <div class="row">
-                    <div class="col-md-6" align="left">
+                    <div class="col-md-6" align="justify">
+                        <li>
+                        At Bellas Prestige Homecare, we believe in empowering individuals to age gracefully in their homes, with dignity and independence.
+                    
+                    </li> 
+                    <li>
+                        Our passion is delivering top-notch care that improves lives - let us make a difference in yours.
+                    
+                    </li>
                     <li>
                         Experience the difference with our compassionate and highly trained caregivers, dedicated to providing exceptional care in the comfort of your home.
                         
@@ -50,14 +58,6 @@
                         With our comprehensive services and flexible scheduling, we have got you covered - choose us for peace of mind.
                     
                     </li>
-                    <li>
-                        At Bellas Prestige Homecare, we believe in empowering individuals to age gracefully in their homes, with dignity and independence.
-                    
-                    </li>
-                    <li>
-                        Our passion is delivering top-notch care that improves lives - let us make a difference in yours.
-                    
-                    </li>
                 </div>
                <div class="col-lg-6 col-md-6">
                         <div class="blog-item">
@@ -68,11 +68,11 @@
                                         <a href="#" class="my-auto"><i class="fas fa-link fa-2x text-white"></i></a>
                                     </div>
                                 </div>
-                                <div class="blog-info d-flex align-items-center border border-start-0 border-end-0">
+                               <!--  <div class="blog-info d-flex align-items-center border border-start-0 border-end-0">
                                     <small class="flex-fill text-center border-end py-2"><i class="fa fa-calendar-alt text-primary me-2"></i>28 Jan 2050</small>
                                     <a href="#" class="btn-hover flex-fill text-center text-white border-end py-2"><i class="fa fa-thumbs-up text-primary me-2"></i>1.7K</a>
                                     <a href="#" class="btn-hover flex-fill text-center text-white py-2"><i class="fa fa-comments text-primary me-2"></i>1K</a>
-                                </div>
+                                </div> -->
                             </div>
                             <!-- <div class="blog-content border border-top-0 rounded-bottom p-4">
                                 <p class="mb-3">Posted By: Royal Hamblin </p>
@@ -85,7 +85,7 @@
             </div>
         </div>
         <div class="text-center">
-            <h5 class="section-title text-center mb-3">Home Care Services</h5>
+            <h3 class="section-title text-center mb-3">&nbsp Our Home Care Services &nbsp</h3>
         </div>
                 <div class="row g-4 justify-content-center">
                     <div class="col-lg-4 col-md-6">
@@ -294,18 +294,18 @@
                         </div>
                     </div>
                 <div class="mx-auto text-center mb-5">
-                    <h5 class="section-title px-3">Our Vision and Mission</h5>
+                    <h3 class="section-title px-3">Our Vision and Mission</h3>
                     <!-- <h1 class="mb-4">About Our Home Care</h1> -->
                     <div class="row">
-                    <div class="col-md-5" align="left">
+                    <div class="col-md-5" align="justify">
                         <h3 style="text-decoration: underline; font-weight: bolder;">Vision</h3>
                         <p class="mb-0">"To be the leading provider of home care services, delivering exceptional domestic help, nanny, and caregiving solutions that enhance the quality of life for families and individuals, fostering a supportive and nurturing environment where every client feels valued and  cared for."
                     </p>
                     </div>
-                    <div class="col-md-7" align="left">
+                    <div class="col-md-7" align="justify">
                         <h3 style="text-decoration: underline; font-weight: bolder;">Mission</h3>
                         <p class="mb-0">
-                            At Bellas Prestige Limited, our mission is to offer reliable, compassionate, and professional home care services that cater for the unique needs of our clients. We are dedicated to:
+                            At Bellas Prestige Homecare, our mission is to offer reliable, compassionate, and professional home care services that cater for the unique needs of our clients. We are dedicated to:
                             <li>providing highly trained and trustworthy domestic Assistance who help with household tasks to ensure a clean, orderly and comfortable living environment.
                             </li>
                             <li>offering experienced and nurturing nannies who support the developmental and emotional well-being of children in a safe and stimulating setting.
