554dd77b48403c446a2d4b0b42607961945bfc29
Francis Nkansah <itmannanayaw@gmail.com>
2024-07-27 21:31:24 -0700

    changes in homecare

--- homecare.php
+++ homecare.php
@@ -17,11 +17,11 @@
         <!-- Header Start -->
         <div class="container-fluid bg-breadcrumb">
             <div class="container text-center py-5" style="max-width: 900px;">
-                <h3 class="text-white display-3 mb-4">Our Blog</h1>
+                <h3 class="text-white display-3 mb-4">Our Home Care Services</h1>
                 <ol class="breadcrumb justify-content-center mb-0">
                     <li class="breadcrumb-item"><a href="index.php">Home</a></li>
                     <li class="breadcrumb-item"><a href="#">Pages</a></li>
-                    <li class="breadcrumb-item active text-white">Blog</li>
+                    <li class="breadcrumb-item active text-white">Home Care</li>
                 </ol>    
             </div>
         </div>
@@ -30,12 +30,61 @@
         <!-- Blog Start -->
         <div class="container-fluid blog py-5">
             <div class="container py-5">
-                <div class="mx-auto text-center mb-5" style="max-width: 900px;">
-                    <h5 class="section-title px-3">Our Blog</h5>
-                    <h1 class="mb-4">Popular Travel Blogs</h1>
-                    <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti deserunt tenetur sapiente atque. Magni non explicabo beatae sit, vel reiciendis consectetur numquam id similique sunt error obcaecati ducimus officia maiores.
+                <div class="mx-auto text-center mb-5">
+                    <h5 class="section-title text-center px-3">Home Care Services</h5>
+                    <h1 class="mb-4">Why choose us</h1>
+                    <div class="row">
+                    <div class="col-md-6" align="left">
+                    <li>
+                        <p class="">Experience the difference with our compassionate and highly trained caregivers, dedicated to providing exceptional care in the comfort of your home.
+                        </p>
+                    </li>
+                    <li>
+                        <p class="">Trust our team to deliver peronalized care with dignity, respected, and kindness, exceeding your expectations.
                     </p>
+                    </li>
+                    <li>
+                        <p class="">Our commitment to quality care and customer satisfaction sets us apart - let us be your partner in healthcare.
+                    </p>
+                    </li>
+                    <li>
+                        <p class="">With our comprehensive services and flexible scheduling, we have got you covered - choose us for peace of mind.
+                    </p>
+                    </li>
+                    <li>
+                        <p class="">At Bellas Prestige Homecare, we believe in empowering individuals to age gracefully in their homes, with dignity and independence.
+                    </p>
+                    </li>
+                    <li>
+                        <p class="">Our passion is delivering top-notch care that improves lives - let us make a difference in yours.
+                    </p>
+                    </li>
                 </div>
+               <div class="col-lg-6 col-md-6">
+                        <div class="blog-item">
+                            <div class="blog-img">
+                                <div class="blog-img-inner">
+                                    <img class="img-fluid w-100 rounded-top" src="img/homecare/homecare.jpg" alt="Image">
+                                    <div class="blog-icon">
+                                        <a href="#" class="my-auto"><i class="fas fa-link fa-2x text-white"></i></a>
+                                    </div>
+                                </div>
+                                <div class="blog-info d-flex align-items-center border border-start-0 border-end-0">
+                                    <small class="flex-fill text-center border-end py-2"><i class="fa fa-calendar-alt text-primary me-2"></i>28 Jan 2050</small>
+                                    <a href="#" class="btn-hover flex-fill text-center text-white border-end py-2"><i class="fa fa-thumbs-up text-primary me-2"></i>1.7K</a>
+                                    <a href="#" class="btn-hover flex-fill text-center text-white py-2"><i class="fa fa-comments text-primary me-2"></i>1K</a>
+                                </div>
+                            </div>
+                            <!-- <div class="blog-content border border-top-0 rounded-bottom p-4">
+                                <p class="mb-3">Posted By: Royal Hamblin </p>
+                                <a href="#" class="h4">Adventures Trip</a>
+                                <p class="my-3">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam eos</p>
+                                <a href="#" class="btn btn-primary rounded-pill py-2 px-4">Read More</a>
+                            </div> -->
+                        </div>
+                    </div>
+            </div>
+        </div>
                 <div class="row g-4 justify-content-center">
                     <div class="col-lg-4 col-md-6">
                         <div class="blog-item">
@@ -106,6 +155,28 @@
                             </div>
                         </div>
                     </div>
+                     <div class="mx-auto text-center mb-5" style="max-width: 900px;">
+                    <h5 class="section-title px-3">About our home care</h5>
+                    <!-- <h1 class="mb-4">About Our Home Care</h1> -->
+                    <div class="row">
+                    <div class="col-md-6" align="left">
+                        <h3>Our Vision</h3>
+                        <p class="mb-0">"To be the leading provider of home care services, delivering exceptional domestic help, nanny, and caregiving solutions that enhance the quality of life for families and individuals, fostering a supportive and nurturing environment where every client feels valued and  cared for."
+                    </p>
+                    </div>
+                    <div class="col-md-6" align="left">
+                        <h3>Our Mission</h3>
+                        <p class="mb-0">
+                            At Bellas Prestige Limited, our mission is to offer reliable, compassionate, and professional home care services that cater for the unique needs of our clients. We are dedicated to:
+                            <li>providing highly trained and trustworthy domestic Assistance who help with household tasks to ensure a clean, orderly and comfortable living environment.
+                            </li>
+                            <li>offering experienced and nurturing nannies who support the developmental and emotional well-being of children in a safe and stimulating setting.
+                            </li>
+                            <li>delivering skilled and empathetic caregiving services that promote the health, independence, and dignity of the elderly and individuals with special needs. With our clients, our aim is to establish enduring relationships founded on mutual respect, trust, and a strong dedication to providing exceptional home care.</li>
+                        </p>
+                    </div>
+                    </div>
+                </div>
                 </div>
             </div>
         </div>
