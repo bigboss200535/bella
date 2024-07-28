b3ea48c820e1fecadd22e76e80d048555b275d33
Francis Nkansah <itmannanayaw@gmail.com>
2024-07-28 12:47:29 -0700

    changes made in property

--- property.php
+++ property.php
@@ -43,23 +43,23 @@
                     <div class="blog-item">
                             <div class="blog-img">
                                 <div class="blog-img-inner">
-                                    <img class="img-fluid w-100 rounded-top" src="img/blog-1.jpg" alt="Image">
+                                    <img class="img-fluid rounded-top" style="width:95%; height: 40%;" src="img/blog-1.jpg" alt="Image">
                                     <div class="blog-icon">
                                         <a href="#" class="my-auto"><i class="fas fa-link fa-2x text-white"></i></a>
                                     </div>
                                 </div>
-                                <div class="blog-info d-flex align-items-center border border-start-0 border-end-0">
+                               <!--  <div class="blog-info d-flex align-items-center border border-start-0 border-end-0">
                                     <small class="flex-fill text-center border-end py-2"><i class="fa fa-calendar-alt text-primary me-2"></i>28 Jan 2050</small>
                                     <a href="#" class="btn-hover flex-fill text-center text-white border-end py-2"><i class="fa fa-thumbs-up text-primary me-2"></i>1.7K</a>
                                     <a href="#" class="btn-hover flex-fill text-center text-white py-2"><i class="fa fa-comments text-primary me-2"></i>1K</a>
-                                </div>
+                                </div> -->
                             </div>
-                            <div class="blog-content border border-top-0 rounded-bottom p-4">
+                            <!-- <div class="blog-content border border-top-0 rounded-bottom p-4">
                                 <p class="mb-3">Posted By: Royal Hamblin </p>
                                 <a href="#" class="h4">Adventures Trip</a>
                                 <p class="my-3">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam eos</p>
                                 <a href="#" class="btn btn-primary rounded-pill py-2 px-4">Read More</a>
-                            </div>
+                            </div> -->
                         </div>
                 </div>
             </div>
