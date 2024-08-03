b295bd7bb01174c272a95edacd34894a6e58a6db
Francis Nkansah <itmannanayaw@gmail.com>
2024-08-03 07:59:43 -0700

    our_guide changes

--- include/our_guide.php
+++ include/our_guide.php
@@ -1,7 +1,7 @@
 <div class="container-fluid guide py-5">
             <div class="container py-5">
                 <div class="mx-auto text-center mb-5" style="max-width: 900px;">
-                    <h5 class="section-title px-3">Travel Guide</h5>
+                    <h5 class="section-title px-3">Our Guide</h5>
                     <h1 class="mb-0">Meet Our Guide</h1>
                 </div>
                 <div class="row g-4">
