2484d011b94cebcd534e4d7242e04deb5ada557d
Francis Nkansah <itmannanayaw@gmail.com>
2024-08-12 23:30:01 -0700

    travel and style modified

--- travel.php
+++ travel.php
@@ -13,13 +13,13 @@
         <!-- Navbar & Hero End -->
 
         <!-- Header Start -->
-        <div class="container-fluid bg-estate">
+        <div class="container-fluid bg-travel">
             <div class="container text-center py-5" style="max-width: 900px;">
-                <h3 class="text-white display-3 mb-4">Travel</h1>
+                <h3 class="text-white display-3 mb-4">Travel & Tour</h1>
                 <ol class="breadcrumb justify-content-center mb-0">
                     <li class="breadcrumb-item"><a href="index.php">Home</a></li>
                     <li class="breadcrumb-item"><a href="#">Pages</a></li>
-                    <li class="breadcrumb-item active text-white">About</li>
+                    <li class="breadcrumb-item active text-white">Travel & Tour</li>
                 </ol>    
             </div>
         </div>
@@ -565,7 +565,23 @@
         <!-- Blog End -->
 
         <!-- Subscribe Start -->
-        <?php include('include/news_letter.php');?>
+        <div class="container-fluid subscribe-travel py-5">
+            <div class="container text-center py-5">
+                <div class="mx-auto text-center" style="max-width: 900px;">
+                    <h5 class="subscribe-title px-3">Subscribe</h5>
+                    <h1 class="text-white mb-4">Our Newsletter</h1>
+                    <p class="text-white mb-5">
+                        Kindly subscribe to our newsletter, so that we bring you the latest updates on travels and more....  
+                    </p>
+                    <div class="position-relative mx-auto">
+                        <form id="newsletter_submission">
+                            <input class="form-control border-primary rounded-pill w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
+                            <button type="button" class="btn btn-gold rounded-pill position-absolute top-0 end-0 py-2 px-4 mt-2 me-2">Subscribe</button>
+                         </form>
+                    </div>
+                </div>
+            </div>
+        </div>
         <!-- Subscribe End -->
         <!-- Footer Start -->
         <?php include('include/footer.php');?>
