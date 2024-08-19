4ba085e701ceeaed80c25cb770a5421d78dc40ce
Francis Nkansah <itmannanayaw@gmail.com>
2024-08-18 19:28:27 -0700

    style modified

--- include/our_guide.php
+++ include/our_guide.php
@@ -21,7 +21,28 @@
                             <div class="guide-title text-center rounded-bottom p-4">
                                 <div class="guide-title-inner">
                                     <h4 class="mt-3">Michael Kosono</h4>
-                                    <!-- <p class="mb-0">General Manager</p> -->
+                                    <p class="mb-0">General Manager</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <div class="guide-item">
+                            <div class="guide-img">
+                                <div class="guide-img-efects">
+                                    <img src="img/home/eric.png" class="img-fluid w-100 rounded-top" alt="Image">
+                                </div>
+                                <div class="guide-icon rounded-pill p-2">
+                                    <a class="btn btn-square btn-primary rounded-circle mx-1" href=""><i class="fab fa-facebook-f"></i></a>
+                                    <a class="btn btn-square btn-primary rounded-circle mx-1" href=""><i class="fab fa-twitter"></i></a>
+                                    <a class="btn btn-square btn-primary rounded-circle mx-1" href=""><i class="fab fa-instagram"></i></a>
+                                    <a class="btn btn-square btn-primary rounded-circle mx-1" href=""><i class="fab fa-linkedin-in"></i></a>
+                                </div>
+                            </div>
+                            <div class="guide-title text-center rounded-bottom p-4">
+                                <div class="guide-title-inner">
+                                    <h4 class="mt-3">Eric Berchie</h4>
+                                    <p class="mb-0">Assistant General Manager</p>
                                 </div>
                             </div>
                         </div>
@@ -42,7 +63,7 @@
                             <div class="guide-title text-center rounded-bottom p-4">
                                 <div class="guide-title-inner">
                                     <h4 class="mt-3">Caleb Ohene</h4>
-                                    <!-- <p class="mb-0">I.T Staff</p> -->
+                                    <p class="mb-0">I.T Staff</p>
                                 </div>
                             </div>
                         </div>
@@ -63,7 +84,7 @@
                             <div class="guide-title text-center rounded-bottom p-4">
                                 <div class="guide-title-inner">
                                     <h4 class="mt-3">Derrick Boateng</h4>
-                                    <!-- <p class="mb-0">I.T Staff</p> -->
+                                    <p class="mb-0">I.T Staff & Driver</p>
                                 </div>
                             </div>
                         </div>
@@ -84,7 +105,7 @@
                             <div class="guide-title text-center rounded-bottom p-4">
                                 <div class="guide-title-inner">
                                     <h4 class="mt-3">Kofi Asare</h4>
-                                    <!-- <p class="mb-0">I.T Staff</p> -->
+                                    <p class="mb-0">I.T Staff</p>
                                 </div>
                             </div>
                         </div>
