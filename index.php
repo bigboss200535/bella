0537b4144c06a6d16b39448ee9351d72a9403d89
Mohammed Alhassan <bigboss200535@gmail.com>
2024-09-07 13:23:26 +0000

    Index update

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 	date_default_timezone_set('Africa/Accra');
 	$current_date = date('Y-m-d');
-	$expiry_date = date('2024-09-30');
+	$expiry_date = date('2024-09-06');
 
 	//include('include/head.php');
 
@@ -17,7 +17,7 @@
 		echo '</br>';
 		echo '</br>';
 		echo '</br>';
-		echo "<div id='spinner' align='center' class='show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center' style='background-color:#DAA520'>
+		echo "<div id='spinner' align='center' class='show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center' style='background-color:#ffffff'>
                 <div class='spinner-border text-primary' style='width: 3rem; height: 3rem;'' role='status'>
                      <span class='sr-only'>Page is Updating...</span> 
                </div>
